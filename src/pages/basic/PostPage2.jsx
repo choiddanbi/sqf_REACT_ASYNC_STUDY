fc6a038cb17649041d7aab50d194c7ab761a5ae6
choiddanbi <eksql5955@naver.com>
2024-07-24 17:42:11 +0900

    240724

--- src/pages/basic/PostPage2.jsx
+++ src/pages/basic/PostPage2.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { COLOR_OPTIONS, SIZE_OPTIONS } from '../../constants/productOptions';
 import axios from 'axios';
 
@@ -10,7 +10,37 @@ function PostPage2(props) {
         sizeId: "",
         colorId: ""
     });
-    
+
+    const [ sizeOptions, setSizeOptions ] = useState([]);
+    const [ colorOptions, setColorOptions ] = useState([]);
+
+
+    // 8080(springboot) 에 값 보냄
+    useEffect(() => {
+        const getSizes = async () => {
+            const response = await axios.get("http://localhost:8080/api/v1/sizes"); // response 는 List로 
+            setSizeOptions(response.data);
+            setProduct(product => ({
+                ...product,
+                sizeId: response.data[0].sizeId // springboot로 보낼때 처음 값으로 보내주려고!! => ReqProductDto(productName=test, price=1234, sizeId=1, colorId=1)
+            }));
+        }
+
+        const getColors = async () => {
+            const response = await axios.get("http://localhost:8080/api/v1/colors");
+            setColorOptions(response.data);
+            setProduct(product => ({
+                ...product,
+                colorId: response.data[0].colorId
+            }));
+        }
+
+        getSizes();
+        getColors();
+
+    }, []);
+
+
     // input 창에서 받은 값으로 수정
     const handleInputChange = (e) => {
         setProduct(product => {
@@ -25,7 +55,7 @@ function PostPage2(props) {
     // axios.post => 프로미스
     const handleSubmitClick = async () => {
         try {
-            const response = await axios.post("http://localhost:8080/basic/product", product); // then과 같은 기능 (비동기처리)
+            const response = await axios.post("http://localhost:8080/api/v1/product", product); // then과 같은 기능 (비동기처리)
             console.log(response);
         } catch(error) {
             console.log(error);
@@ -53,19 +83,19 @@ function PostPage2(props) {
                 </p>
                 <p>
                     <label htmlFor="">사이즈</label>
-                    <select name="sizeId" onChange={handleInputChange}>
+                    <select name="sizeId" onChange={handleInputChange} value={product.sizeId}>
                         {
-                            SIZE_OPTIONS.map(size => 
-                            <option key={size.id} value={size.id}>{size.name}</option>)
+                            sizeOptions.map(size => 
+                            <option key={size.sizeId} value={size.sizeId}>{size.sizeName}</option>)
                         }             
                     </select>
                 </p>
                 <p>
                     <label htmlFor="">색상</label>
-                    <select name="colorId" onChange={handleInputChange}>
+                    <select name="colorId" onChange={handleInputChange} value={product.colorId}>
                         {
-                            COLOR_OPTIONS.map(color => 
-                            <option key={color.id} value={color.id}>{color.name}</option>)
+                           colorOptions.map(color => 
+                            <option key={color.colorId} value={color.colorId}>{color.colorName}</option>)
                         }
                     </select>
                 </p>
